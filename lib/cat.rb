c22dbb3f41e5a3f00df9e6f31fe5bae05a370043
fabiola mendoza <rofadalua@hotmail.com>
2019-09-20 18:58:59 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,2 +1,22 @@
 class Cat
-end  # code goes here
+  
+  attr_accessor :owner, :mood
+  attr_reader :name 
+  
+  @@all = []
+  
+  def initialize(name, owner)
+  @name = name 
+  @owner = owner 
+  @mood = 'nervous'
+  @@all << self 
+  
+  end 
+  
+ def self.all 
+   @@all
+end
+
+
+end
+end 
